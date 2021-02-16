3c1d2c0e5f2b6b0c535db12ff091efdd6873071e
DAve <autyjd@gmail.com>
2021-02-16 20:02:19 +0000

    Update task-3.2.3.js

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -10,5 +10,24 @@ var message = greet('Hello', 'Luke');
 /* EXAMPLE */
 
 // ADD YOUR CODE BELOW HERE
+function fullName(firstName, lastName) {
+  return (firstName + lastName);
+}
+
+var person1 = fullName("Tricia", "Auty");
+
+console.log(fullName("Tricia", "Auty"));
+console.log(fullName("Kate", "Auty"));
+console.log(fullName("Michael", "Auty"));
+
+
+console.log(person1);
+
+
+
+
+
+
+
 
 // ADD YOUR CODE ABOVE HERE
