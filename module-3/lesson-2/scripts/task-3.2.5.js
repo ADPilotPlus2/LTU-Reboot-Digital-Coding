1b206d7b58d3063ee4a3a4d5aae3feb0ed6a6ee1
DAve <autyjd@gmail.com>
2021-02-16 20:31:47 +0000

    Update task-3.2.5.js
    More else - need to fix the logic

---
+++ module-3/lesson-2/scripts/task-3.2.5.js
@@ -0,0 +1,21 @@
+
+var temperature = 12;
+
+if (temperature <= 19) {
+    console.log("Put a coat on");
+}else{
+    console.log("It's grand out");
+}
+
+
+if (temperature <0){
+    console.log("Stay inside");
+}else if (temperature <15){
+    console.log("Wear a coat and hat");
+}
+
+if (temperature >22){
+    console.log("Wear a coat and hat");
+}else{
+    console.log("Just pants and vest is fine");
+}
