d2961079063829c2aa96c193d5305253b2eb48e0
DAve <autyjd@gmail.com>
2021-02-16 20:47:30 +0000

    Update task-3.2.6.js
    Using more boolean - And

---
+++ module-3/lesson-2/scripts/task-3.2.6.js
@@ -0,0 +1,9 @@
+var temperature = 12;
+var location = "home";
+
+if (temperature <0 && location === "home") {
+    console.log("Stay inside");
+}else if (temperature <15){
+    console.log("Wear a coat and hat");
+}
+
