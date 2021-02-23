f8ad70334bffa65d3140d928fb4b2300bfb74626
DAve <autyjd@gmail.com>
2021-02-23 19:35:59 +0000

    task 3.4.1
    for loopa

---
+++ module-3/lesson-4/scripts/task-3.4.1.js
@@ -0,0 +1,15 @@
+
+console.log("Hello")
+
+/*for(var i=1;i<=10;i++) {
+    console.log(i)
+}
+*/
+for (var i=1; i<=12; i++) {
+//console.log( i);//
+var multiplyBy = 7;
+var result = i*multiplyBy;
+console.log(result);
+console.log(`$(i)*$(multiplyBy)+$(result)`)
+
+}
