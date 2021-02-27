83637e7c5e91bb4e20f5c1187610a2994f1df65f
DAve <autyjd@gmail.com>
2021-02-27 21:21:04 +0000

    task 3.4.5
    finally got the syntax to force the function to run by putting () after .letsCook

--- module-3/lesson-4/scripts/task-3.4.5.js
+++ module-3/lesson-4/scripts/task-3.4.5.js
@@ -1,4 +1,4 @@
-console.log("Hi, encore un fois");
+console.log("Hi, 3.4.5");
 
 
 const favouriteRecipe = {
@@ -6,31 +6,13 @@ const favouriteRecipe = {
     servings: 6,
     ingredients: ["paste", "meat", "tomato"],
     directions:["make", "taste", "eat"],
-    letsCook: function giveTitle (favouriteRecipe) {
-        var thisRecipeTitle = favouriteRecipe.recipeTitle;
-        return thisRecipeTitle;
+    letsCook: function(){
+        
+        console.log("I'm hungry! Let's cook a "+ favouriteRecipe.recipeTitle);
     }
-
 }
-
-
-console.log("I'm hungry! Let's cook a "+ favouriteRecipe.giveTitle(favouriteRecipe));
-
-
-
-/*console.log(favouriteRecipe.recipeTitle);
-
-for (var i = 0; i<=favouriteRecipe.ingredients.length; i++) {
-
-    let ingredients = favouriteRecipe.ingredients;
-    console.log(ingredients[i]);
-}
-for (var i = 0; i<=favouriteRecipe.directions.length; i++) {
-    
-    let directions = favouriteRecipe.directions;
-    console.log(directions[i]);
-}
+favouriteRecipe.letsCook();
 
 
 
-*/
+
