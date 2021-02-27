bcce11fb41474d8fcd19566e21e64675f684b0ea
DAve <autyjd@gmail.com>
2021-02-27 11:27:32 +0000

    3.5.3
    reading array with function with multiple parameters

---
+++ module-3/lesson-5/scripts/task-3.5.3.js
@@ -0,0 +1,26 @@
+console.log("Hello 3.5.3");
+
+function    discountAmount(arr, discountAmount, type) {
+    let totalPrice=0;
+
+    for (let index=0; index<arr.length; index++) {
+
+        if( arr[index].type=== type ) {
+            let discount = (arr[index].price*discountAmount)/100;
+            totalPrice=totalPrice+(arr[index].price-discount)*arr[index].quantity;
+
+        }else if (arr[index].type==="any")  {
+            let discount = (arr[index].price*discountAmount)/100;
+            totalPrice=totalPrice+(arr[index].price-discount)*arr[index].quantity;
+
+    } else {
+        totalPrice=totalPrice+(arr[index].price*arr[index].quantity);
+    }
+
+    }
+return totalPrice.toFixed(2);
+
+}
+
+console.log(discountAmount(shoppingCart,15,"food"));
+
