7371ba24c70740044038d3a0a87aaebc81e11697
DAve <autyjd@gmail.com>
2021-03-02 21:28:02 +0000

    3.6.3
    event listeners

---
+++ module-3/lesson-6/scripts/task-3.6.3.js
@@ -0,0 +1,12 @@
+console.log("3.6.3");
+
+
+let button=document.querySelector("button");
+
+//let alert =function() {
+//   alert("hiya");
+//} use this if you want to re-use this function
+
+button.addEventListener("mouseover", function() {
+  alert("Hi");
+});
