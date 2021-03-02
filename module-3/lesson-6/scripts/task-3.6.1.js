b9cdbaf9e0eac960fa777598beaba0cd3b0e9703
DAve <autyjd@gmail.com>
2021-03-02 19:50:22 +0000

    task 3.6.1
    query selector on nodes

---
+++ module-3/lesson-6/scripts/task-3.6.1.js
@@ -0,0 +1,11 @@
+console.log("Heloo 3.6.1");
+
+let paragraphs=document.querySelectorAll("p");
+
+let paragraph3 = paragraphs[3];
+    paragraph3.style.border="4px solid red";
+    paragraph3.style.padding = "20px";
+
+let logo = document.querySelector("img");
+
+logo.setAttribute("src", "https://www.istockphoto.com/photo/avro-vulcan-gm153549334-17132896?utm_campaign=srp_photos_noresults&utm_content=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Favro%2F&utm_medium=affiliate&utm_source=pexels&utm_term=avro");
