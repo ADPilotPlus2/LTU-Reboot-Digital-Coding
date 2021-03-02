fc77328f6f279b56835d7b86eaf02f44ebed1616
DAve <autyjd@gmail.com>
2021-03-02 18:49:44 +0000

    exploring input to webpages
    Using input element,, button, function and event listener

--- module-3/lesson-5/scripts/task-3.5.3b.js
+++ module-3/lesson-5/scripts/task-3.5.3b.js
@@ -1,6 +1,13 @@
 console.log("Hello 3.5.3b");
 var arrItems=[];
+var lowPrice =0;
+
 function priceRange (arr,lowPrice,highPrice) {
+    var arr=shoppingCart;
+    var lowPrice = document.getElementById("lowPrice").value;
+    console.log(lowPrice);
+    var highPrice=5;
+    console.log(highPrice);
     for (let index=0; index<arr.length;index++) {
         if (arr[index].price >= lowPrice && arr[index].price <= highPrice) {
             arrItems.push (arr[index].name);
@@ -9,8 +16,18 @@ function priceRange (arr,lowPrice,highPrice) {
 return arrItems;
 }
 
-priceRange(shoppingCart,2,5);
-
+//priceRange(shoppingCart,2,5);
+var text = "waiting for input";
+document.getElementById("demo").innerHTML = text;
+text="";
+document.querySelector(".btn").addEventListener("click", () => {
 for (i=0;i<arrItems.length;i++) {
+    console.log(arrItems.length);
     console.log(arrItems[i]);
+    text += arrItems[i] + "<br>";
 }
+document.getElementById("demo").innerHTML = text;
+arrItems=[];
+});
+//document.getElementById("demo").innerHTML = text;
+
